62143fd263e5ccab61b03b8a497ab93f1fea0b0f
EnigmaticTanmay <59219724+EnigmaticTanmay@users.noreply.github.com>
2020-07-23 23:25:59 +0530

    added all the plots and the code files

---
+++ plot 3.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(tools)
+
+# make sure the .txt file is in your wd
+file = file.path(getwd(), "household_power_consumption.txt")
+tbl <- read.table(file = file, dec = ".", stringsAsFactors = FALSE, colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), header = TRUE, sep = ";", na.strings = "?")
+
+# subset data to free space
+data <- filter(tbl, Date %in% c("1/2/2007", "2/2/2007"))
+rm(tbl)
+
+# new column date_time for x axis
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data <- mutate(data, date_time = as.POSIXct(paste(data$Date, data$Time, sep=" "), template = "%d/%m/%Y %H:%M:%S", tz = Sys.timezone()))
+
+png("plot3.png", width=480, height=480)
+with(data, 
+     { plot(x=date_time, y = Sub_metering_1, type="l", col = "black", xlab="", ylab="Energy sub metering")
+         lines(x=date_time, y = Sub_metering_2, type="l", col = "red")
+         lines(x=date_time, y = Sub_metering_3, type="l", col = "blue")
+         legend("topright", lty = "solid", col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+     })
+dev.off()
