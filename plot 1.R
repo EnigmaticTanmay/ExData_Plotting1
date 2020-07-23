3f3eace8c8a8a8f96fa04eb3b94dc1e00a6b2174
EnigmaticTanmay <59219724+EnigmaticTanmay@users.noreply.github.com>
2020-07-23 23:20:44 +0530

    added plot 1.r and plot1.png

---
+++ plot 1.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(tools)
+
+# make sure the .txt file is in your wd
+file = file.path(getwd(), "household_power_consumption.txt")
+tbl <- read.table(file = file, dec = ".", stringsAsFactors = FALSE, colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), header = TRUE, sep = ";", na.strings = "?")
+
+# subset data to free space
+data <- filter(tbl, Date %in% c("1/2/2007", "2/2/2007"))
+rm(tbl)
+
+png("plot1.png", width=480, height=480)
+hist(data$Global_active_power, col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)", ylim = c(0,1200), 
+     xlim = c(0,6), breaks = 12)
+dev.off()
+
